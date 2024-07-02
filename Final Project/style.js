18b3f1ac3d1bc6fefbc2ea2e7ea991bc88db841d
SweetChris-uc <sweetchrisuc@gmail.com>
2024-07-02 12:45:08 +0100

    Add files via upload

---
+++ Final Project/style.js
@@ -0,0 +1,17 @@
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
+
+const Element = document.querySelector('.list-link')
+      Element.classList.add('hideOnMobile')
+
+
+
+const element = document.getElementById('header')
+      element.style.backgroundColor = "white"
